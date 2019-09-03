d8f97be8d6796f2b0c24b3e8a5a6fd8b311f87ca
CristianQS <cristian_31teror@hotmail.com>
2019-09-03 01:10:56 +0100

    fix prod db uri

--- src/configs/db.js
+++ src/configs/db.js
@@ -4,7 +4,7 @@ module.exports.connectDb = () => {
   mongoose.set('useFindAndModify', false)
   if(process.env.NODE_ENV === 'production') {
     return mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME_PROD}:${process.env.DB_PASSWORD_PROD}`+
-    `@apicrm-jmu6p.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true })
+    `@apicrm-jmu6p.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true })
   }
   return mongoose.connect(`mongodb://${process.env.MONGO_URI}:${process.env.MONGO_PORT}/`+
   `${process.env.DB_NAME}`, { useNewUrlParser: true })
